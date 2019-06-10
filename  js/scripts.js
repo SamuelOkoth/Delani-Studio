dd16174e961c765b86e9e9be3e6bbc93228f1e53
SamuelOkoth <okothsamuel78@gmail.com>
2019-06-10 15:13:15 +0300

    Changed the link

---  js/scripts.js
+++  js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $(".img-fluid").click(function() {
-    $(".img-fluid").hide();
+    $(".img-fluid").toggle();
+    $('.hide').toggle();
   });
 });
 $('.form-cont').submit(function(event) {
