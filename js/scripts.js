872781d06cd431f1ddf155f56f12e03e4e8c3e2b
SamuelOkoth <okothsamuel78@gmail.com>
2019-06-11 15:39:23 +0300

    changed my scripts to the main file due to problems with linking

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,51 @@
-$(document).ready(function() {
-  $(".img-fluid").click(function() {
-    $(".img-fluid").toggle();
-    $('.hide').toggle();
+/ what we do section
+$('.main').click(function () {
+  $(this).children('.hide').slideToggle(1200);
+  $(this).children('.show').toggle(1500);
+});
+// giving feedback to the customer
+$("button").click(function (e) {
+  var client = document.getElementById('name').value;
+  alert(' Dear ' + ' esteemed ' + client + ' we have received your message. ' + ' Thank you for the feedback.');
+  e.preventDefault();
+});
+
+// capturing the input details in form
+
+$("#form").submit(function (e) {
+  e.preventDefault();
+  var name = $("input#name").val();
+  console.log(name);
+});
+
+$("#form").submit(function (e) {
+  e.preventDefault();
+  var email = $("input#email").val();
+  console.log(email);
+});
+
+$("#form").submit(function (e) {
+  e.preventDefault();
+  var message = $("textarea#message").val();
+  console.log(message);
+});
+//color of text in form
+$('input#name').css('color', 'white');
+$('input#email').css('color', 'white');
+$('textarea#message').css('color', 'white');
+
+
+//    reset my form button
+$("button").on('click', function () {
+  $('form').each(function () {
+    this.reset();
   });
 });
-$('.form-cont').submit(function(event) {
-  var name = $('input#name').val();
-  var email = $('input#email').val();
-  if (name && email != '') {
-    alert('Thank you so much for contacting us  ' + name + ' we appreciate your feedback');
-    alert("Your message has been received!")
-  } else {
-    alert('Invalid input');
-  }
-  event.preventDefault();
+//portfolio section 
+$(".card").hover(function () {
+  $(this).children(".portfolio").fadeToggle(1000, "linear");
 });
+Collapse
+
+
+
